799e4c9504feb69f675113bd259253574a8962f6
Anton Tetov <anton@tetov.se>
2021-06-15 21:09:52 +0200

    Need to have more than 0 tests.

---
+++ tests/test_import.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+def test_import():
+    import compas_convert  # noqa: F401
+    import compas_convert.install  # noqa: F401
+    import compas_convert.native  # noqa: F401
+    import compas_convert.rhino  # noqa: F401
+    import compas_convert.smart_converter  # noqa: F401
